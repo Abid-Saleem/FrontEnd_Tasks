47ce07e2b5ed2f88ef2395e233e673b5b3de7201
Abid Ali <abidali.bhl@gmail.com>
2023-06-04 17:35:54 +0500

    Detail Pages

--- Job-Circle-Theme/markup/js/jquery.main.js
+++ Job-Circle-Theme/markup/js/jquery.main.js
@@ -54,8 +54,42 @@ function initSlickCarousel() {
 		    {
 				breakpoint: 768,
 				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+		    },
+		    {
+				breakpoint: 576,
+				settings: {
 					slidesToShow: 1,
-					slidesToScroll: 2,
+					slidesToScroll: 1,
+				},
+		    },
+		],
+	});
+
+	jQuery('.similar-slider').slick({
+		slidesToScroll: 1,
+		slidesToShow: 3,
+		rows: 0,
+		arrows: false,
+		dots: true,
+		focusOnSelect: false,
+		infinite: true,
+		autoplay: true,
+		responsive: [
+		    {
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+		    },
+		    {
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
 				},
 		    },
 		],
