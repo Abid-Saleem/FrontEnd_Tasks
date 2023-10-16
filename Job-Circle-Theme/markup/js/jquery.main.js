3402fc8a9cccd094147d3e4d78bac23f6e5c8ddf
Irfan-Ayub09 <99260040+Irfan-Ayub09@users.noreply.github.com>
2023-10-16 15:11:11 +0500

    Theme-16

--- Job-Circle-Theme/markup/js/jquery.main.js
+++ Job-Circle-Theme/markup/js/jquery.main.js
@@ -290,6 +290,51 @@ function initSlickCarousel() {
 		],
 	});
 
+	jQuery('.recent_candidate_slider').slick({
+		slidesToScroll: 1,
+		slidesToShow: 4,
+		rows: 0,
+		arrows: false,
+		dots: true,
+		focusOnSelect: false,
+		infinite: true,
+		autoplay: true,
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					arrows: false,
+				},
+		    },
+		    {
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					arrows: false,
+				},
+		    },
+		    {
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					arrows: false,
+				},
+		    },
+		    {
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+				},
+		    },
+		],
+	});
+
 	jQuery('.client_testimonials_slider').slick({
 		slidesToScroll: 1,
 		slidesToShow: 3,
